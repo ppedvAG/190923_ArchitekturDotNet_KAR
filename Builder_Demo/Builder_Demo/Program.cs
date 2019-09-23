ba58a0ac12eeebaed58df9d4c4a48062fb89f001
Michael Zöhling <michael.zoehling@outlook.com>
2019-09-23 12:00:47 +0200

    Builder

--- Builder_Demo/Builder_Demo/Program.cs
+++ Builder_Demo/Builder_Demo/Program.cs
@@ -10,6 +10,25 @@ namespace Builder_Demo
     {
         static void Main(string[] args)
         {
+
+            var Ikea1 = Schrank.BaueSchrank()
+                               .MitBöden(2)
+                               .MitTüren(4)
+                               .MitOberfläche(Oberflächenart.Lackiert)
+                               .InFarbe("Gelb")
+                               .Konstruiere();
+            
+            var Ikea2 = Schrank.BaueSchrank()
+                               .MitBöden(6)
+                               .MitTüren(2)
+                               .MitOberfläche(Oberflächenart.Gewachst)
+                               .MitKleiderstange(false)
+                               .MitMetallschiene(true)
+                               .Konstruiere();
+
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
         }
     }
 }
