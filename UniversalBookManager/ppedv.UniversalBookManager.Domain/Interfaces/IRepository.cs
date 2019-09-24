a0fd248ddb2c90f93a9cda4bc1525eb14a67e565
Michael Zöhling <michael.zoehling@outlook.com>
2019-09-24 11:38:50 +0200

    Schnittstelle für das Repository erstellt

---
+++ UniversalBookManager/ppedv.UniversalBookManager.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ppedv.UniversalBookManager.Domain.Interfaces
+{
+    public interface IRepository
+    {
+        void Add<T>(T item) where T : Entity; // == nur unsere eigenen Datentypen
+        void Update<T>(T item) where T : Entity;
+        void Delete<T>(T item) where T : Entity;
+        T GetByID<T>(int ID) where T : Entity;
+        IEnumerable<T> GetAll<T>() where T : Entity;
+        void Save();
+    }
+}
