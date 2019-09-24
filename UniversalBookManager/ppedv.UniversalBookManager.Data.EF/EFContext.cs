5882ae2c07f79b5246f499be136e08c3628b0012
Michael Zöhling <michael.zoehling@outlook.com>
2019-09-24 10:20:38 +0200

    Basiskonfiguration für EF erstellt

---
+++ UniversalBookManager/ppedv.UniversalBookManager.Data.EF/EFContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.Entity;
+using ppedv.UniversalBookManager.Domain;
+
+namespace ppedv.UniversalBookManager.Data.EF
+{
+    public class EFContext : DbContext // Konfiguration für das EF
+    {
+        // "Update" auf 6.2 ;)
+
+        // Für TN: (Richtige SQL-DB)
+        // "Server=.;Database=UBM;Trusted_Connection=true"
+
+        // Trainerrechner: SQL-Express Edition (minimaler SQL-Server auf LocalDB-Basis)
+        public EFContext() : this(@"Server=(localdb)\MSSQLLocalDB;Database=UBM;Trusted_Connection=true;AttachDbFilename=C:\temp\UBM.mdf") { }
+        public EFContext(string connectionString) : base(connectionString) { } // Parameter aus dem eigenen Konstruktor wird an den Konstruktor der Basisklasse weitergegeben
+
+        public DbSet<Book> Book { get; set; }
+        public DbSet<Store> Store { get; set; }
+        public DbSet<Inventory> Inventory { get; set; }
+    }
+}
