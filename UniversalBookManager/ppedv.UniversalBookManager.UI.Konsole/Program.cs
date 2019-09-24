db0c780c955dc1504de64d88e7e481cdb3d34005
Michael Zöhling <michael.zoehling@outlook.com>
2019-09-24 12:16:39 +0200

    Mini-UI erstellt

--- UniversalBookManager/ppedv.UniversalBookManager.UI.Konsole/Program.cs
+++ UniversalBookManager/ppedv.UniversalBookManager.UI.Konsole/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ppedv.UniversalBookManager.Data.EF;
+using ppedv.UniversalBookManager.Domain.Interfaces;
+using ppedv.UniversalBookManager.Logic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +13,19 @@ namespace ppedv.UniversalBookManager.UI.Konsole
     {
         static void Main(string[] args)
         {
+            Core core = new Core(new EFRepository(new EFContext()));
+
+            // 2) Testdaten generieren, wenn keine Testdaten existieren
+            if(core.GetAllBooks().Length == 0) // nicht besonders perfomant ;)
+                core.GenerateTestData();
+
+            foreach (var book in core.GetAllBooks())
+            {
+                Console.WriteLine($"{book.Author}: {book.Title}, Preis: {book.Price}");
+            }
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
         }
     }
 }
