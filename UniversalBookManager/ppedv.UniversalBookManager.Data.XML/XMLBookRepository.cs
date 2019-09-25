49a3314c118024fd48ff9ae927ac9a936877f8af
Michael Zöhling <michael.zoehling@outlook.com>
2019-09-25 10:59:53 +0200

    Primitiv-Variante für die XML-Datalayer erstellt

---
+++ UniversalBookManager/ppedv.UniversalBookManager.Data.XML/XMLBookRepository.cs
@@ -0,0 +1,61 @@
+﻿using ppedv.UniversalBookManager.Domain;
+using ppedv.UniversalBookManager.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace ppedv.UniversalBookManager.Data.XML
+{
+    public class XMLBookRepository : IBookRepository
+    {
+        public IEnumerable<Book> GetAll()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Book[]));
+
+            FileStream stream = new FileStream("Books.xml", FileMode.Open);
+            Book[] data = (Book[])serializer.Deserialize(stream);
+            stream.Close();
+
+            return data;
+        }
+
+        #region ToDo: XML-Features implementieren
+        public void Add(Book item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Book item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book GetBookWithHighestPrice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book GetByID(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetTotalAmountOfBooksInCirculation(Book b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Book> Query()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Book item)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ UniversalBookManager/ppedv.UniversalBookManager.Data.XML/XMLUnitOfWOrk.cs
@@ -0,0 +1,25 @@
+﻿using ppedv.UniversalBookManager.Domain;
+using ppedv.UniversalBookManager.Domain.Interfaces;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.UniversalBookManager.Data.XML
+{
+    public class XMLUnitOfWork : IUnitOfWork
+    {
+        public IBookRepository BookRepository => throw new NotImplementedException();
+
+        public IStoreRepository StoreRepository => throw new NotImplementedException();
+
+        public IUniversalRepository<T> GetRepository<T>() where T : Entity
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
