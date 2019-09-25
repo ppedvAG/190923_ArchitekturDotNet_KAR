60b5c466d2f68e3ffcbb8e80cda750ffc71315f8
Michael Zöhling <michael.zoehling@outlook.com>
2019-09-25 10:53:49 +0200

    Init Data.XML

---
+++ UniversalBookManager/ppedv.UniversalBookManager.Data.XML/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.UniversalBookManager.Data.XML
+{
+    public class Class1
+    {
+    }
+}
