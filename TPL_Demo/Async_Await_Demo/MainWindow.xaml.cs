abfab373367550bcedb990b739a3c23224df11d6
Michael Zöhling <michael.zoehling@outlook.com>
2019-09-25 14:46:34 +0200

    Timer, Worker, DispatcherTimer etc...

--- TPL_Demo/Async_Await_Demo/MainWindow.xaml.cs
+++ TPL_Demo/Async_Await_Demo/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -13,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Async_Await_Demo
 {
@@ -24,7 +26,41 @@ namespace Async_Await_Demo
         public MainWindow()
         {
             InitializeComponent();
+            // t = new Timer(UhrzeitStellen,null,0,1000);
+
+            //BackgroundWorker worker = new BackgroundWorker();
+            // worker.DoWork += WorkerMachtWas;
+
+            //worker.RunWorkerAsync();
+
+            DispatcherTimer timer = new DispatcherTimer(); // Quasi wie Timer inkl UI-Thread (somit kein Dispatcher.Invoke notwendig !!!!)
+            timer.Interval = TimeSpan.FromSeconds(3);
+            timer.Tick += DispatcherTimer_Tick;
+
+            timer.Start();
+
+            // Parallel
+            // Auf 2 Kerne beschränken:
+            // Parallel.For(0, 100, new ParallelOptions { MaxDegreeOfParallelism = 2 }, i => { });
+        
         }
+
+        private void DispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            textBlockUhr.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        //private void WorkerMachtWas(object sender, DoWorkEventArgs e)
+        //{
+        //    MessageBox.Show("Worker macht was ...");
+        //}
+
+        //private Timer t;
+
+        //private void UhrzeitStellen(object state)
+        //{
+        //    Dispatcher.Invoke(() => textBlockUhr.Text = DateTime.Now.ToLongTimeString());
+        //}
 
         public event EventHandler TaskBeendetEvent;
 
@@ -42,6 +78,21 @@ namespace Async_Await_Demo
             textBoxEingabe.Text = "Nach dem Task"; // Zugriff auf ein Element vom UI-Thread aus
             // ^ mit ConfigureAwait -> Exception, weil wir hier nicht mehr im UI-Thread sind !!!!!
 
+
+            // async Task
+            // async Task<T>
+            // async void  <==== Rückwärtskompatiblität für EventHandler (signatur ist: void(object,eventargs))
+
+            // Beispiel:
+
+            string demo = await Task<string>.Run(() =>
+            {
+                return DateTime.Now.ToLongTimeString();
+            });
+
+            await Task.Delay(5000); // 5 sec warten vor dem ergebnis
+            MessageBox.Show(demo);
+
             MessageBox.Show("Ende");
         }
 
@@ -51,7 +102,7 @@ namespace Async_Await_Demo
             {
                 for (int i = 0; i <= 100; i++)
                 {
-                    Thread.Sleep(200);
+                    Thread.Sleep(100);
                     Dispatcher.Invoke(() => progressBarWert.Value = i);
                     // "UI-Thread...bitte mach für mich xyz"
                 }
